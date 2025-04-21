5ca77c4e598f3254f41ccddcd3a6b69f2c12c68a
difahrahman <difa.habiba.rahman@gmail.com>
2025-04-22 06:28:01 +0700

    Add Join Club page design

--- src/store/club.ts
+++ src/store/club.ts
@@ -4,10 +4,25 @@ export const CLUB_DUMMY = [
     id: 1,
     name: "Club 1",
     logo: "/images/logo.png",
+    owner: "Owner 1",
   },
   {
     id: 2,
     name: "Club 2",
     logo: "/images/logo.png",
+    owner: "Owner 2",
+  },
+];
+
+export const CLUB_INVITE_DUMMY = [
+  {
+    id: 1,
+    name: "Club 1",
+    sport_name: "Basketball",
+  },
+  {
+    id: 2,
+    name: "Club 2",
+    sport_name: "Football",
   },
 ];
