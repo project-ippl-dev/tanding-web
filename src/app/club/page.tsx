56898954520602e812f2f1ace5c86cbcee8f1f16
difahrahman <difa.habiba.rahman@gmail.com>
2025-04-22 21:21:50 +0700

    WIP: Club detail page design

--- src/app/club/page.tsx
+++ src/app/club/page.tsx
@@ -1,5 +1,4 @@
 "use client";
-import PageTitleSection from "@/components/common/PageTitleSection";
 import { CLUB_DUMMY, CLUB_INVITE_DUMMY } from "@/store/club";
 import { Cancel, CheckCircle } from "@mui/icons-material";
 import {
@@ -18,38 +17,19 @@ import {
   Tabs,
   Typography,
 } from "@mui/material";
-import { useState } from "react";
-
-function TabPanel(props) {
-  const { children, value, index, ...other } = props;
-
-  return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`simple-tabpanel-${index}`}
-      aria-labelledby={`simple-tab-${index}`}
-      {...other}
-    >
-      {value === index && (
-        <Box p={3}>
-          <Typography>{children}</Typography>
-        </Box>
-      )}
-    </div>
-  );
-}
+import { SyntheticEvent, useState } from "react";
+import TabPanel from "./_components/TabPanel";
 
 export default function ClubPage() {
   const [page, setPage] = useState<number>(1);
   const [tabValue, setTabValue] = useState<number>(0);
-  const handleTab = (e, newValue: number) => {
+  const handleTab = (e: SyntheticEvent, newValue: number) => {
     setTabValue(newValue);
   };
 
   // TODO: Connect to BE
   // const handleApproveInvite = (approval_id, status) => {
-  //   
+  //
   // };
 
   // useEffect(() => {
@@ -64,132 +44,127 @@ export default function ClubPage() {
   const club_invite = CLUB_INVITE_DUMMY;
 
   return (
-    <div>
-      <PageTitleSection title="Bergabung dengan Club">
-        Buat atau bergabung dengan club terbaik untukmu dan bermainlah bersama!
-      </PageTitleSection>
-      <Container
-        maxWidth="lg"
-        sx={(theme) => ({
-          paddingTop: "50px",
-          paddingBottom: "50px",
-          [theme.breakpoints.down("md")]: {
-            paddingTop: theme.spacing(10),
-          },
-        })}
-      >
-        <Card>
-          <Tabs value={tabValue} onChange={handleTab}>
-            <Tab label="Club" />
-            <Tab label="Invitation" />
-          </Tabs>
-          <TabPanel value={tabValue} index={0}>
-            <Box>
-              <Table>
-                <TableHead>
-                  <TableRow>
-                    <TableCell>No.</TableCell>
-                    <TableCell>Club</TableCell>
-                    <TableCell>Leaders</TableCell>
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  {club.map((value, index) => (
-                    <TableRow
-                      key={value.id}
-                      sx={{
-                        cursor: "pointer",
-                      }}
-                      // TODO: Connect to club detail
-                      // onClick={() => router.push(`/club/${value.id}`)}
-                    >
-                      <TableCell>{index + 1}</TableCell>
-                      <TableCell>
-                        <Box display="flex" alignItems="center">
-                          <Avatar
-                            src={value.logo}
-                            sx={{
-                              marginRight: "10px",
-                            }}
-                          />
-                          <Typography>{value.name}</Typography>
-                        </Box>
-                      </TableCell>
-                      <TableCell>{value.owner}</TableCell>
-                    </TableRow>
-                  ))}
-                  {club.length === 0 && (
-                    <TableRow>
-                      <TableCell colSpan={3} align="center">
-                        Tidak ada data club
-                      </TableCell>
-                    </TableRow>
-                  )}
-                </TableBody>
-              </Table>
-            </Box>
-          </TabPanel>
-          <TabPanel value={tabValue} index={1}>
+    <Container
+      maxWidth="lg"
+      sx={(theme) => ({
+        paddingTop: "50px",
+        paddingBottom: "50px",
+        [theme.breakpoints.down("md")]: {
+          paddingTop: theme.spacing(10),
+        },
+      })}
+    >
+      <Card>
+        <Tabs value={tabValue} onChange={handleTab}>
+          <Tab label="Club" />
+          <Tab label="Invitation" />
+        </Tabs>
+        <TabPanel value={tabValue} index={0}>
+          <Box>
             <Table>
               <TableHead>
                 <TableRow>
-                  <TableCell>No</TableCell>
+                  <TableCell>No.</TableCell>
                   <TableCell>Club</TableCell>
-                  <TableCell>Olahraga</TableCell>
-                  <TableCell>Action</TableCell>
+                  <TableCell>Leaders</TableCell>
                 </TableRow>
               </TableHead>
               <TableBody>
-                {club_invite.map((value, index) => (
-                  <TableRow key={value.id}>
+                {club.map((value, index) => (
+                  <TableRow
+                    key={value.id}
+                    sx={{
+                      cursor: "pointer",
+                    }}
+                    // TODO: Connect to club detail
+                    // onClick={() => router.push(`/club/${value.id}`)}
+                  >
                     <TableCell>{index + 1}</TableCell>
-                    <TableCell>{value.name}</TableCell>
-                    <TableCell>{value.sport_name}</TableCell>
                     <TableCell>
-                      <IconButton
-                        sx={{
-                          "&:hover": {
-                            color: "green",
-                          },
-                        }}
-                        // onClick={() => handleApproveInvite(value.id, true)}
-                      >
-                        <CheckCircle />
-                      </IconButton>
-                      <IconButton
-                        sx={{
-                          "&:hover": {
-                            color: "red",
-                          },
-                        }}
-                        // onClick={() => handleApproveInvite(value.id, false)}
-                      >
-                        <Cancel />
-                      </IconButton>
+                      <Box display="flex" alignItems="center">
+                        <Avatar
+                          src={value.logo}
+                          sx={{
+                            marginRight: "10px",
+                          }}
+                        />
+                        <Typography>{value.name}</Typography>
+                      </Box>
                     </TableCell>
+                    <TableCell>{value.owner}</TableCell>
                   </TableRow>
                 ))}
-                {club_invite.length === 0 && (
+                {club.length === 0 && (
                   <TableRow>
-                    <TableCell colSpan={4} align="center">
-                      Tidak ada undangan club
+                    <TableCell colSpan={3} align="center">
+                      Tidak ada data club
                     </TableCell>
                   </TableRow>
                 )}
               </TableBody>
             </Table>
-          </TabPanel>
-          <Box marginBottom={2}>
-            <Pagination
-              page={page}
-              // count={club.all.last_page}
-              count={10}
-              color="primary"
-              onChange={(e, newValue) => setPage(newValue)}
-            />
           </Box>
-        </Card>
-      </Container>
-    </div>
+        </TabPanel>
+        <TabPanel value={tabValue} index={1}>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>No</TableCell>
+                <TableCell>Club</TableCell>
+                <TableCell>Olahraga</TableCell>
+                <TableCell>Action</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {club_invite.map((value, index) => (
+                <TableRow key={value.id}>
+                  <TableCell>{index + 1}</TableCell>
+                  <TableCell>{value.name}</TableCell>
+                  <TableCell>{value.sport_name}</TableCell>
+                  <TableCell>
+                    <IconButton
+                      sx={{
+                        "&:hover": {
+                          color: "green",
+                        },
+                      }}
+                      // onClick={() => handleApproveInvite(value.id, true)}
+                    >
+                      <CheckCircle />
+                    </IconButton>
+                    <IconButton
+                      sx={{
+                        "&:hover": {
+                          color: "red",
+                        },
+                      }}
+                      // onClick={() => handleApproveInvite(value.id, false)}
+                    >
+                      <Cancel />
+                    </IconButton>
+                  </TableCell>
+                </TableRow>
+              ))}
+              {club_invite.length === 0 && (
+                <TableRow>
+                  <TableCell colSpan={4} align="center">
+                    Tidak ada undangan club
+                  </TableCell>
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
+        </TabPanel>
+        <Box marginBottom={2}>
+          <Pagination
+            page={page}
+            // count={club.all.last_page}
+            count={10}
+            color="primary"
+            onChange={(e, newValue) => setPage(newValue)}
+          />
+        </Box>
+      </Card>
+    </Container>
   );
 }
--- src/store/club.ts
+++ src/store/club.ts
@@ -5,12 +5,49 @@ export const CLUB_DUMMY = [
     name: "Club 1",
     logo: "/images/logo.png",
     owner: "Owner 1",
+    short_name: 'C1',
+    joined: true,
+    privilege: true,
+    member: [
+      {
+        id: 1,
+        name: 'Member 1',
+        point: 100
+      },
+      {
+        id: 2,
+        name: 'Member 2',
+        point: 85
+      },
+      {
+        id: 3,
+        name: 'Member 3',
+        point: 0
+      }
+    ],
+    join: [
+      {
+        id: 1,
+        name: 'Calon Member 1',
+      },
+      {
+        id: 2,
+        name: 'Calon Member 2',
+      },
+      {
+        id: 3,
+        name: 'Calon Member 3',
+      }
+    ]
   },
   {
     id: 2,
     name: "Club 2",
     logo: "/images/logo.png",
     owner: "Owner 2",
+    short_name: 'C2',
+    joined: true,
+    privilege: false,
   },
 ];
 
