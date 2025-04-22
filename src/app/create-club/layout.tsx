54e1776dddfed7fc1053998b58eb6c0912ec3b7d
difahrahman <difa.habiba.rahman@gmail.com>
2025-04-23 00:19:32 +0700

    Add Create Club page design and connect to nav

--- src/app/create-club/layout.tsx
+++ src/app/create-club/layout.tsx
@@ -1,14 +1,14 @@
 import PageTitleSection from "@/components/common/PageTitleSection";
 
-export default function ClubManagementLayout({
+export default function ClubCreateLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
     <div>
-      <PageTitleSection title="Join Club">
-        Bergabung dengan club terbaik untukmu dan bermainlah bersama!
+      <PageTitleSection title="Buat Club">
+        Pastikan seluruh form terisi dengan lengkap dan jelas demi keaslian data
       </PageTitleSection>
       {children}
     </div>
