7d2f5a9c094d0bc21be72b53d20d2648787ece90
Difa Rahman <difa.habiba.rahman@gmail.com>
2025-04-23 15:24:40 +0700

    Revert "feat: login-register MVP"

--- src/app/register/layout.tsx
+++
@@ -1,9 +0,0 @@
-// src/app/(auth)/layout.tsx
-export default function AuthLayout({ children }: { children: React.ReactNode }) {
-    return (
-      <div style={{ backgroundColor: "#fafafa", minHeight: "100vh" }}>
-        {children}
-      </div>
-    );
-  }
-  
